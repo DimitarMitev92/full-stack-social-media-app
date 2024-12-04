27913eaed5520620aa182e390c7d8e2f19a3d48d
Dimitar Mitev <d.mitev920123@gmail.com>
2024-12-04 20:08:20 +0200

    feat: FriendRequest component

---
+++ src/components/FriendRequests.tsx
@@ -0,0 +1,105 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const FriendRequests = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* TOP */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">Friend Requests</span>
+        <Link href="/" className="text-blue-500 text-sm">
+          See all
+        </Link>
+      </div>
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/19127257/pexels-photo-19127257/free-photo-of-woman-sitting-on-a-rug-in-a-living-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <span className="font-semibold">Wayne Burton</span>
+        </div>
+        <div className="flex justify-end gap-3">
+          <Image
+            src="/accept.png"
+            alt=""
+            width={20}
+            height={20}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/reject.png"
+            alt=""
+            width={20}
+            height={20}
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/19127257/pexels-photo-19127257/free-photo-of-woman-sitting-on-a-rug-in-a-living-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <span className="font-semibold">Wayne Burton</span>
+        </div>
+        <div className="flex justify-end gap-3">
+          <Image
+            src="/accept.png"
+            alt=""
+            width={20}
+            height={20}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/reject.png"
+            alt=""
+            width={20}
+            height={20}
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/19127257/pexels-photo-19127257/free-photo-of-woman-sitting-on-a-rug-in-a-living-room.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full object-cover"
+          />
+          <span className="font-semibold">Wayne Burton</span>
+        </div>
+        <div className="flex justify-end gap-3">
+          <Image
+            src="/accept.png"
+            alt=""
+            width={20}
+            height={20}
+            className="cursor-pointer"
+          />
+          <Image
+            src="/reject.png"
+            alt=""
+            width={20}
+            height={20}
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FriendRequests;
